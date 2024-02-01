5b3942a07895cf253065d078249669febec49f93
Bharat-HoiChoi <139767455+Bharat-HoiChoi@users.noreply.github.com>
2024-02-01 08:59:55 +0530

    No Changes

--- composeApp/src/commonMain/kotlin/di/KoinDi.kt
+++ composeApp/src/commonMain/kotlin/di/KoinDi.kt
@@ -27,7 +27,6 @@ val composeAppModule = module {
     single { NotesRepositoryImpl(get()) } bind NotesRepository::class
     single { AuthViewModel(get(),get()) }
     single { NotesViewModel(get()) }
-    //asdasdasd
 }
 
 fun initComposeKoin(
